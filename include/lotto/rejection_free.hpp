a51351e036104d64ee30a83b5ef7c4fc7c447669
bpuchala <bpuchala@users.noreply.github.com>
2024-10-21 11:58:04 -0400

    Allow custom get_impact function in rejection free selector

--- include/lotto/rejection_free.hpp
+++ include/lotto/rejection_free.hpp
@@ -15,11 +15,26 @@ class RejectionFreeEventSelectorTest;
 
 namespace lotto {
 
+template <typename EventIDType>
+struct GetImpactFromTable {
+  GetImpactFromTable(
+      std::map<EventIDType, std::vector<EventIDType>> const &_impact_table)
+      : impact_table(_impact_table) {}
+
+  std::vector<EventIDType> const &operator()(
+      const EventIDType &event_id) const {
+    return impact_table.at(event_id);
+  }
+
+  std::map<EventIDType, std::vector<EventIDType>> const &impact_table;
+};
+
 /*
  * Event selector implemented using rejection-free KMC algorithm
  */
 template <typename EventIDType, typename RateCalculatorType,
-          typename EngineType = std::mt19937_64>
+          typename EngineType = std::mt19937_64,
+          typename GetImpactType = GetImpactFromTable<EventIDType>>
 class RejectionFreeEventSelector
     : public EventSelectorBase<EventIDType, RateCalculatorType, EngineType> {
  public:
@@ -28,14 +43,33 @@ class RejectionFreeEventSelector
   RejectionFreeEventSelector(
       const std::shared_ptr<RateCalculatorType> &rate_calculator_ptr,
       const std::vector<EventIDType> &event_id_list,
-      const std::map<EventIDType, std::vector<EventIDType> > &impact_table,
-      std::shared_ptr<RandomGeneratorT<EngineType> > random_generator =
-          std::shared_ptr<RandomGeneratorT<EngineType> >())
+      const std::map<EventIDType, std::vector<EventIDType>> &impact_table,
+      std::shared_ptr<RandomGeneratorT<EngineType>> random_generator =
+          std::shared_ptr<RandomGeneratorT<EngineType>>())
       : EventSelectorBase<EventIDType, RateCalculatorType, EngineType>(
             rate_calculator_ptr, random_generator),
         event_rate_tree(event_id_list, this->calculate_rates(event_id_list)),
         impact_table(fill_impact_table(impact_table, event_id_list)),
-        impacted_events_ptr(nullptr) {
+        impacted_events_ptr(nullptr),
+        get_impact(impact_table) {
+    if (event_id_list.empty()) {
+      throw std::runtime_error("Event ID list must not be empty.");
+    }
+  }
+
+  // Construct given a rate calculator, event ID list, get impact function, and
+  // random number generator
+  RejectionFreeEventSelector(
+      const std::shared_ptr<RateCalculatorType> &rate_calculator_ptr,
+      const std::vector<EventIDType> &event_id_list, GetImpactType get_impact_f,
+      std::shared_ptr<RandomGeneratorT<EngineType>> random_generator =
+          std::shared_ptr<RandomGeneratorT<EngineType>>())
+      : EventSelectorBase<EventIDType, RateCalculatorType, EngineType>(
+            rate_calculator_ptr, random_generator),
+        event_rate_tree(event_id_list, this->calculate_rates(event_id_list)),
+        impact_table(),
+        impacted_events_ptr(nullptr),
+        get_impact(get_impact_f) {
     if (event_id_list.empty()) {
       throw std::runtime_error("Event ID list must not be empty.");
     }
@@ -76,17 +110,21 @@ class RejectionFreeEventSelector
   EventRateTree<EventIDType> event_rate_tree;
 
   // Lookup table indicating, for a given event that is accepted, which events'
-  // rates are impacted
-  const std::map<EventIDType, std::vector<EventIDType> > impact_table;
+  // rates are impacted (optional, only used if impact_table is provided to
+  // the constructor)
+  const std::map<EventIDType, std::vector<EventIDType>> impact_table;
 
   // Pointer to vector of impacted events whose rates have not been updated
   mutable const std::vector<EventIDType> *impacted_events_ptr;
+
+  // Function object to get impacted events from the accepted event ID
+  GetImpactType get_impact;
 
   // Set the impact events pointer based on an accepted event ID
   void set_impacted_events(const EventIDType &accepted_event_id) {
     assert(impacted_events_ptr ==
            nullptr);  // pointer should be null before proceeding
-    impacted_events_ptr = &impact_table.at(accepted_event_id);
+    impacted_events_ptr = &get_impact(accepted_event_id);
     return;
   }
 
@@ -103,8 +141,8 @@ class RejectionFreeEventSelector
 
   // Add missing event IDs to an impact table (with empty vectors as values)
   // and return it
-  std::map<EventIDType, std::vector<EventIDType> > fill_impact_table(
-      std::map<EventIDType, std::vector<EventIDType> > table_to_fill,
+  std::map<EventIDType, std::vector<EventIDType>> fill_impact_table(
+      std::map<EventIDType, std::vector<EventIDType>> table_to_fill,
       std::vector<EventIDType> event_id_list) {
     for (const EventIDType &event_id : event_id_list) {
       table_to_fill[event_id];
@@ -115,6 +153,7 @@ class RejectionFreeEventSelector
   // Friend for testing
   friend class ::RejectionFreeEventSelectorTest;
 };
+
 }  // namespace lotto
 
 #endif
